c31944b41e72f68951665f30571209a15f9ef3d9
Olivier Le Merdy <olivier.lemerdy@kry.se>
2024-12-04 17:26:38 +0100

    Initial commit

---
+++ src/main/java/se/kry/demo/DependabotSecurityDemoApplication.java
@@ -0,0 +1,13 @@
+package se.kry.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DependabotSecurityDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DependabotSecurityDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se/kry/demo/DependabotSecurityDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package se.kry.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DependabotSecurityDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
